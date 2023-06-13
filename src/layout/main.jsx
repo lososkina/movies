835d74c35b0c7a5b06c43aa81d7eb6a7761b3235
lososkina <loskinaa2004@gmail.com>
2023-06-14 01:43:30 +0300

    los1

---
+++ src/layout/main.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Movies } from "../components/Movies";
+import { Search } from "../components/search";
+import { Preloader } from "../components/preloader";
+
+class Main extends React.Component {
+    state = {
+        movies: [],
+        loading: true,
+    };
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=8ea852d6&s=Matrix')
+            .then((response) => response.json())
+            .then((data) => this.setState({ movies: data.Search, loading: false }));
+    }
+
+    searchMovies = (str, type = "all") => {
+        fetch(
+            `http://www.omdbapi.com/?apikey=8ea852d6&s=${str}${type !== "all" ? `&type=${type}` : ""
+            }`
+        )
+            .then((response) => response.json())
+            .then((data) => this.setState({ movies: data.Search, loading: false }));
+    };
+
+    render() {
+        const { loading } = this.state;
+        const style = { position: "fixed", left: "50%", transform: "translate(-50%, -50%)" };
+
+        return (
+            <main className="container content">
+                <Search searchMovies={this.searchMovies} />
+                {loading ? <Preloader /> : <Movies movies={this.state.movies} />}
+            </main>
+        );
+    }
+}
+export { Main };
